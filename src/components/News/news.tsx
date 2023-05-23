5a516582c3cf5ad1848ba722430a9796b7814e75
Martin Kariuki <martinkariuki7@gmail.com>
2023-05-23 13:38:03 +0300

    Netlify error fix

--- src/components/News/news.tsx
+++ src/components/News/news.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { formatDate } from "../../utils/FormatDate";
 import styles from "./news.module.css";
 
