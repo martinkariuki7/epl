b7a9f169dfc1f58c6329201f73b17f406c2dd846
Martin Kariuki <martinkariuki7@gmail.com>
2023-05-25 10:55:03 +0300

    Add badge to Table standings

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,6 +11,7 @@ import {
   checkLastFiveGames,
   getTeamPoints,
 } from "../../utils/TableStanding";
+import { Match } from "../../pages/Matches";
 import styles from "./Table.module.css";
 
 interface sortColumn {
@@ -21,7 +22,7 @@ interface sortColumn {
 interface Props {
   teams: Team[];
   handleTeamMatches: HandleTeamMatches;
-  fixtures: any[];
+  fixtures: Match[];
 }
 
 const Table = ({ teams, handleTeamMatches, fixtures }: Props) => {
@@ -43,11 +44,6 @@ const Table = ({ teams, handleTeamMatches, fixtures }: Props) => {
       });
     }
   };
-
-  // Update the JSX for the table headers
-  <th onClick={() => handleSort("matchesPlayed")} scope="col">
-    MP
-  </th>;
 
   // Ugly code, consider refactoring
   const sorted = _.orderBy(
@@ -96,84 +92,104 @@ const Table = ({ teams, handleTeamMatches, fixtures }: Props) => {
   );
 
   return (
-    <table className={`table table-hover ${styles.table}`}>
-      <thead>
-        <tr>
-          <th scope="col">Club</th>
-          <th colSpan={8}></th>
-          <th onClick={() => handleSort("MP")} scope="col">
-            MP
-          </th>
-          <th onClick={() => handleSort("W")} scope="col">
-            W
-          </th>
-          <th onClick={() => handleSort("D")} scope="col">
-            D
-          </th>
-          <th onClick={() => handleSort("L")} scope="col">
-            L
-          </th>
-          <th onClick={() => handleSort("GF")} scope="col">
-            GF
-          </th>
-          <th onClick={() => handleSort("GA")} scope="col">
-            GA
-          </th>
-          <th onClick={() => handleSort("GD")} scope="col">
-            GD
-          </th>
-          <th onClick={() => handleSort("Pts")} scope="col">
-            Pts
-          </th>
-          <th scope="col" className="d-flex justify-content-center">
-            Last 5
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {sorted.map((team: Team, index: number) => {
-          const {
-            title: { rendered: team_name },
-            acf: { team_color },
-          } = team;
-          return (
-            <tr
-              key={team.id}
-              onClick={() => handleTeamMatches(team.id, team_color, team_name)}
-            >
-              <th>{index + 1}</th>
-              <td className={styles.teamBranding} colSpan={8}>
-                <img
-                  src={team.fimg_url ? team.fimg_url : ""}
-                  alt={team.title.rendered + "logo"}
-                />
-                {team.title.rendered}
-              </td>
-              <td>{getMatchesPlayed(team.id, fixtures)}</td>
-              <td>{getMatchesWon(team.id, fixtures)}</td>
-              <td>{getMatchesDrawn(team.id, fixtures)}</td>
-              <td>{getMatchesLost(team.id, fixtures)}</td>
-              <td>{getTeamGoalsFor(team.id, fixtures)}</td>
-              <td>{getTeamGoalsAgainst(team.id, fixtures)}</td>
-              <td>
-                {getTeamGoalsFor(team.id, fixtures) -
-                  getTeamGoalsAgainst(team.id, fixtures)}
-              </td>
-              <td>{getTeamPoints(team.id, fixtures)}</td>
-              <td className={styles.winDrawLost}>
-                <div className="d-flex justify-content-center">
-                  {checkLastFiveGames(team.id, fixtures).map(
-                    (result, index) => (
-                      <span key={index}>{result}</span>
-                    )
-                  )}
-                </div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="table-responsive">
+      <table className={`table table-hover ${styles.table}`}>
+        <thead>
+          <tr>
+            <th></th>
+            <th scope="col">Club</th>
+            <th colSpan={8}></th>
+            <th onClick={() => handleSort("MP")} scope="col">
+              MP
+            </th>
+            <th onClick={() => handleSort("W")} scope="col">
+              W
+            </th>
+            <th onClick={() => handleSort("D")} scope="col">
+              D
+            </th>
+            <th onClick={() => handleSort("L")} scope="col">
+              L
+            </th>
+            <th onClick={() => handleSort("GF")} scope="col">
+              GF
+            </th>
+            <th onClick={() => handleSort("GA")} scope="col">
+              GA
+            </th>
+            <th onClick={() => handleSort("GD")} scope="col">
+              GD
+            </th>
+            <th onClick={() => handleSort("Pts")} scope="col">
+              Pts
+            </th>
+            <th scope="col" className="d-flex justify-content-center">
+              Last 5
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sorted.map((team: Team, index: number) => {
+            const {
+              title: { rendered: team_name },
+              acf: { team_color },
+            } = team;
+            let columnBadge = "";
+
+            if (index < 4) {
+              columnBadge = styles.championsLeague;
+            } else if (index >= sorted.length - 3) {
+              columnBadge = styles.relegation;
+            } else if (index === 4 || index === 5) {
+              columnBadge = styles.europa;
+            } else if (index === 6) {
+              columnBadge = styles.conferenceLeague;
+            }
+
+            return (
+              <tr
+                key={team.id}
+                onClick={() =>
+                  handleTeamMatches(team.id, team_color, team_name)
+                }
+              >
+                <th>
+                  <div className={columnBadge}></div>
+                </th>
+                <th>{index + 1}</th>
+                <td className={styles.teamBranding} colSpan={8}>
+                  <img
+                    src={team.fimg_url ? team.fimg_url : ""}
+                    alt={team.title.rendered + "logo"}
+                  />
+                  {team.title.rendered}
+                </td>
+                <td>{getMatchesPlayed(team.id, fixtures)}</td>
+                <td>{getMatchesWon(team.id, fixtures)}</td>
+                <td>{getMatchesDrawn(team.id, fixtures)}</td>
+                <td>{getMatchesLost(team.id, fixtures)}</td>
+                <td>{getTeamGoalsFor(team.id, fixtures)}</td>
+                <td>{getTeamGoalsAgainst(team.id, fixtures)}</td>
+                <td>
+                  {getTeamGoalsFor(team.id, fixtures) -
+                    getTeamGoalsAgainst(team.id, fixtures)}
+                </td>
+                <td>{getTeamPoints(team.id, fixtures)}</td>
+                <td className={styles.winDrawLost}>
+                  <div className="d-flex justify-content-center">
+                    {checkLastFiveGames(team.id, fixtures).map(
+                      (result, index) => (
+                        <span key={index}>{result}</span>
+                      )
+                    )}
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
